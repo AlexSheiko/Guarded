ac5b818328aa3e539e08823480d183f6c96fe20b
AlexeySheiko <alexeysheiko@hotmail.com>
2016-09-05 00:59:28 +0200

    Initial commit

---
+++ app/src/main/java/android/accessguarded/guarded365/co/uk/guardedguard/MainActivity.java
@@ -0,0 +1,13 @@
+package android.accessguarded.guarded365.co.uk.guardedguard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
