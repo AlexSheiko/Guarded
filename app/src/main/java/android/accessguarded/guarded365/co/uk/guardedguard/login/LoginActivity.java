86b8828f5dc61c77c0a18422e53ce86f05a79d3e
AlexeySheiko <alexeysheiko@hotmail.com>
2016-09-05 18:57:00 +0200

    Submit username and password for validation

--- app/src/main/java/android/accessguarded/guarded365/co/uk/guardedguard/login/LoginActivity.java
+++ app/src/main/java/android/accessguarded/guarded365/co/uk/guardedguard/login/LoginActivity.java
@@ -4,6 +4,8 @@ import android.accessguarded.guarded365.co.uk.guardedguard.R;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -13,10 +15,24 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText mUsernameField;
+    private EditText mPasswordField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mUsernameField = (EditText) findViewById(R.id.usernameField);
+        mPasswordField = (EditText) findViewById(R.id.passwordField);
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateCredentials(
+                        mUsernameField.getText().toString().trim(),
+                        mPasswordField.getText().toString().trim());
+            }
+        });
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.guarded365.co.uk/")
@@ -37,5 +53,9 @@ public class LoginActivity extends AppCompatActivity {
                 t.printStackTrace();
             }
         });
+    }
+
+    private void validateCredentials(String username, String password) {
+
     }
 }
