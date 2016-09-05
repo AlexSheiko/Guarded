ae0b82318cfa93c90bb9cdb006318868df37f01c
AlexeySheiko <alexeysheiko@hotmail.com>
2016-09-05 14:16:42 +0200

    Exclude duplicated files in XML converter

--- app/src/main/java/android/accessguarded/guarded365/co/uk/guardedguard/login/LoginActivity.java
+++ app/src/main/java/android/accessguarded/guarded365/co/uk/guardedguard/login/LoginActivity.java
@@ -3,7 +3,11 @@ package android.accessguarded.guarded365.co.uk.guardedguard.login;
 import android.accessguarded.guarded365.co.uk.guardedguard.R;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class LoginActivity extends AppCompatActivity {
@@ -18,5 +22,18 @@ public class LoginActivity extends AppCompatActivity {
                 .build();
 
         LoginService service = retrofit.create(LoginService.class);
+        Call<User> call = service.login("api_test", "api_test_password");
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User user = response.body();
+                Log.d("LoginActivity", "onResponse: " + user.getUsername());
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 }
