7a5840c6b69ebf08a232062a02b88664c519e893
AlexeySheiko <alexeysheiko@hotmail.com>
2016-09-05 02:39:09 +0200

    Organize activities by feature

---
+++ app/src/main/java/android/accessguarded/guarded365/co/uk/guardedguard/guarddetail/GuardDetailActivity.java
@@ -0,0 +1,14 @@
+package android.accessguarded.guarded365.co.uk.guardedguard.guarddetail;
+
+import android.accessguarded.guarded365.co.uk.guardedguard.R;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GuardDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guard_detail);
+    }
+}
